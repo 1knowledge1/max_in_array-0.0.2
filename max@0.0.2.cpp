8f8aaf726a39813e3b96e8c90dcafc74d49bd08b
Yaroslav <32066631+1knowledge1@users.noreply.github.com>
2017-09-29 23:06:37 +0300

    Create max@0.0.2.cpp

---
+++ max@0.0.2.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+
+int main()
+{
+	bool failure2 = false, failure = false;
+	int max, max2;
+
+	for (std::string string; std::getline(std::cin, string); ) {
+		int numbers[10];
+		std::istringstream stream(string);
+		for (int i = 0; i < 10; ++i) {
+			if (!(stream >> numbers[i])) {
+				failure = true;
+				break;
+			}
+		}
+		max = numbers[0];
+		for (int j = 0; j < 10; j++) {
+			if (numbers[j]>max) {
+				max = numbers[j];
+			}
+		}
+		break;
+	}
+
+	for (std::string string2; std::getline(std::cin, string2); ) {
+		int numbers2[10];
+		std::istringstream stream(string2);
+		for (int i = 0; i < 10; ++i) {
+			if (!(stream >> numbers2[i])) {
+				failure2 = true;
+				break;
+			}
+		}
+		max2 = numbers2[0];
+		for (int j = 0; j < 10; j++) {
+			if (numbers2[j] > max2) {
+				max2 = numbers2[j];
+			}
+		}
+		break;
+	}
+
+	if (!failure && !failure2) {
+		std::cout << max + max2;
+	}
+	else {
+		std::cout << "An error has occured while reading numbers from line" << std::endl;
+		}
+
+	return 0;
+}
